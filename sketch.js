40d94c9a65df6105b9cba1cb4848e1b87f368d10
alexeltonpym <alexeltonpym@gmail.com>
2021-05-19 18:38:31 +1000

    changed to short vid for github pages

--- sketch.js
+++ sketch.js
@@ -70,7 +70,7 @@ function preload() {
   coral_shader = loadShader('coral_base.vert', 'coral_shader.frag');
 
 
-  vid = createVideo('full_bg_video.mp4', () => {
+  vid = createVideo('short_bg_video.mp4', () => {
     vid_loaded = true
     vid.hide();
     vid.loop();
